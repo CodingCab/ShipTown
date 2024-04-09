68515713e44583b498330c89f89f45eddc6fca99
Artur Hanusek <arthur@youritsolutions.ie>
2024-04-09 18:35:56 +0200

    magento2msi-improvements

--- database/seeders/Modules/Magento2MSI/ConnectionSeeder.php
+++ database/seeders/Modules/Magento2MSI/ConnectionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders\Modules\Magento2MSI;
 
 use App\Modules\Magento2MSI\src\Models\Magento2msiConnection;
 use Illuminate\Database\Seeder;
+use Spatie\Tags\Tag;
 
 class ConnectionSeeder extends Seeder
 {
@@ -13,10 +14,16 @@ class ConnectionSeeder extends Seeder
             return;
         }
 
-        Magento2msiConnection::query()->updateOrCreate([
+        $connection = Magento2msiConnection::query()->updateOrCreate([
             'base_url' => env('TEST_MODULES_MAGENTO2MSI_BASE_URL'),
+            'magento_source_code' => env('TEST_MODULES_MAGENTO2MSI_MAGENTO_SOURCE_CODE', 'default'),
         ], [
             'api_access_token' => env('TEST_MODULES_MAGENTO2MSI_ACCESS_TOKEN'),
         ]);
+
+        $tag = Tag::findOrCreate('ALL');
+        $connection->inventory_source_warehouse_tag_id = $tag->id;
+        $connection->save();
+
     }
 }
