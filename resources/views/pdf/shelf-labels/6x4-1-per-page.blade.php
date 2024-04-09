000a0b1c7f25f0b0bd1b4838c872fb52969fa996
Robert Goodliffe <robgoodliffe@gmail.com>
2024-04-09 17:04:50 +0200

    1.0.256

--- resources/views/pdf/shelf-labels/6x4-1-per-page.blade.php
+++ resources/views/pdf/shelf-labels/6x4-1-per-page.blade.php
@@ -3,8 +3,8 @@
 
     @foreach($labels as $label)
         <h1 style="text-align: center; font-size: 85px; margin-top: 10px;">{{ $label }}</h1>
-        <img style="width: 180px; height: 180px; margin-top:100px; margin-left: 88px;" src="data:image/png;base64,{{ DNS2D::getBarcodePNG($label, 'QRCODE') }}" alt="barcode" />
-        <p style="text-align: center; font-size: 24px; margin-top: 20px;">self:{{ $label }}</p>
+        <img style="width: 180px; height: 180px; margin-top:100px; margin-left: 88px;" src="data:image/png;base64,{{ DNS2D::getBarcodePNG('shelf:'.$label, 'QRCODE') }}" alt="barcode" />
+        <p style="text-align: center; font-size: 24px; margin-top: 20px;">{{ $label }}</p>
         @if(!$loop->last)
             <div class="page-break"></div>
         @endif
