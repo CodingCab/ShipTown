0e69b46254625adf43e3e20f620f8b0c2d0bb3d0
Artur Hanusek <arthur@youritsolutions.ie>
2024-04-30 18:23:20 +0200

    exclude users without selected warehouse

--- database/migrations/2024_04_30_162014_fill_users_warehouse_code.php
+++ database/migrations/2024_04_30_162014_fill_users_warehouse_code.php
@@ -7,7 +7,11 @@ return new class extends Migration
 {
     public function up(): void
     {
-        $users = User::query()->whereNull('warehouse_code')->with('warehouse')->get();
+        $users = User::query()
+            ->whereNotNull('warehouse_id')
+            ->whereNull('warehouse_code')
+            ->with('warehouse')
+            ->get();
 
         $users->each(function (User $user) {
             return $user->update(['warehouse_code' => $user->warehouse->code]);
