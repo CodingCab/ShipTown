d4a504e8c2024e3319b1a1c7ef5beae254a73e25
Artur Hanusek <arthur@youritsolutions.ie>
2024-05-12 17:48:54 +0200

    fix tests

--- app/Modules/InventoryTotals/src/Listeners/EveryMinuteEventListener.php
+++ app/Modules/InventoryTotals/src/Listeners/EveryMinuteEventListener.php
@@ -2,7 +2,6 @@
 
 namespace App\Modules\InventoryTotals\src\Listeners;
 
-use App\Modules\InventoryTotals\src\Jobs\RecalculateInventoryRecordsJob;
 use App\Modules\InventoryTotals\src\Jobs\RecalculateInventoryTotalsByWarehouseTagJob;
 use App\Modules\InventoryTotals\src\Jobs\RecalculateInventoryTotalsTableJob;
 
