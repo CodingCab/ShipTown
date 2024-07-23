960c8b2aa73ed415b6e83ab9eb1699545f8d69b9
Patryk Krawczuk <patryk.krawczuk@jootbox.eu>
2024-07-23 13:18:05 +0200

    changed foreign id declaration in migration

--- database/migrations/2024_07_23_104357_create_modules_quantity_discounts_products_table.php
+++ database/migrations/2024_07_23_104357_create_modules_quantity_discounts_products_table.php
@@ -4,16 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('modules_quantity_discounts_products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('quantity_discount_id');
-            $table->foreignId('product_id');
+            $table->unsignedBigInteger('quantity_discount_id');
+            $table->unsignedBigInteger('product_id');
             $table->timestamps();
             $table->softDeletes();
+
+            $table->foreign('quantity_discount_id')
+                ->references('id')
+                ->on('modules_quantity_discounts')
+                ->onDelete('cascade');
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
         });
     }
 
