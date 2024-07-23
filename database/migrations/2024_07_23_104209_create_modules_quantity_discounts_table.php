b9de528c3441f079fed9541830e6dafd941f72d9
Patryk Krawczuk <patryk.krawczuk@jootbox.eu>
2024-07-23 13:01:06 +0200

    fix migrations

--- database/migrations/2024_07_23_104209_create_modules_quantity_discounts_table.php
+++ database/migrations/2024_07_23_104209_create_modules_quantity_discounts_table.php
@@ -9,9 +9,9 @@ return new class extends Migration {
     {
         Schema::create('modules_quantity_discounts', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable()->after('id');
-            $table->string('type', 15)->nullable()->after('name');
-            $table->json('configuration')->nullable()->after('type');
+            $table->string('name')->nullable();
+            $table->string('type', 15)->nullable();
+            $table->json('configuration')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
