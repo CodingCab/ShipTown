c2f3373fd5102970445a1aaa4286c3d224a30a81
Artur Hanusek <arthur@youritsolutions.ie>
2024-07-25 17:29:39 +0200

    fix table name

--- database/migrations/2024_07_25_151321_add_price_columns_to_data_collections_records_table.php
+++ database/migrations/2024_07_25_151321_add_price_columns_to_data_collections_records_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('data_collections_records', function (Blueprint $table) {
+        Schema::table('data_collection_records', function (Blueprint $table) {
             $table->decimal('unit_cost', 10, 2)->nullable()->after('quantity_to_scan');
             $table->decimal('unit_sold_price', 10, 2)->nullable()->after('unit_cost');
             $table->decimal('unit_discount', 10, 2)->nullable()->after('unit_sold_price');
