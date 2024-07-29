5c61febc59a85fcd45488e170e4234998aa1478d
Patryk Krawczuk <patryk.krawczuk@jootbox.eu>
2024-07-29 13:24:16 +0200

    1.0.317

---
+++ app/Modules/QuantityDiscounts/src/Listeners/DataCollectionRecordCreatedEventListener.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Modules\QuantityDiscounts\src\Listeners;
+
+class DataCollectionRecordCreatedEventListener
+{
+
+}
---
+++ app/Modules/QuantityDiscounts/src/Listeners/DataCollectionRecordDeletedEventListener.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Modules\QuantityDiscounts\src\Listeners;
+
+class DataCollectionRecordDeletedEventListener
+{
+
+}
