f54a1a837d68539c3abea0506a3259a03d818f06
Artur Hanusek <arthur@youritsolutions.ie>
2024-08-02 01:05:52 +0200

    pass test

--- app/Modules/QuantityDiscounts/src/Listeners/DataCollectionRecordCreatedEventListener.php
+++ app/Modules/QuantityDiscounts/src/Listeners/DataCollectionRecordCreatedEventListener.php
@@ -3,10 +3,27 @@
 namespace App\Modules\QuantityDiscounts\src\Listeners;
 
 use App\Events\DataCollectionRecord\DataCollectionRecordCreatedEvent;
+use App\Modules\QuantityDiscounts\src\Models\QuantityDiscount;
 
 class DataCollectionRecordCreatedEventListener
 {
     public function handle(DataCollectionRecordCreatedEvent $event)
     {
+        $record = $event->dataCollectionRecord;
+
+        QuantityDiscount::query()
+            ->whereHas('products', function ($query) use ($record) {
+                $query->whereIn('product_id', function ($subQuery) use ($record) {
+                    $subQuery->select('product_id')
+                        ->from('data_collection_records')
+                        ->where('data_collection_id', $record->data_collection_id);
+                });
+            })
+            ->with('products')
+            ->get()
+            ->each(function (QuantityDiscount $quantityDiscount) use ($record) {
+                $job = new $quantityDiscount->job_class($quantityDiscount, $record->dataCollection);
+                $job->handle();
+            });
     }
 }
--- app/Modules/QuantityDiscounts/src/Listeners/DataCollectionRecordDeletedEventListener.php
+++ app/Modules/QuantityDiscounts/src/Listeners/DataCollectionRecordDeletedEventListener.php
@@ -3,10 +3,27 @@
 namespace App\Modules\QuantityDiscounts\src\Listeners;
 
 use App\Events\DataCollectionRecord\DataCollectionRecordDeletedEvent;
+use App\Modules\QuantityDiscounts\src\Models\QuantityDiscount;
 
 class DataCollectionRecordDeletedEventListener
 {
     public function handle(DataCollectionRecordDeletedEvent $event)
     {
+        $record = $event->dataCollectionRecord;
+
+        QuantityDiscount::query()
+            ->whereHas('products', function ($query) use ($record) {
+                $query->whereIn('product_id', function ($subQuery) use ($record) {
+                    $subQuery->select('product_id')
+                        ->from('data_collection_records')
+                        ->where('data_collection_id', $record->data_collection_id);
+                });
+            })
+            ->with('products')
+            ->get()
+            ->each(function (QuantityDiscount $quantityDiscount) use ($record) {
+                $job = new $quantityDiscount->job_class($quantityDiscount, $record->dataCollection);
+                $job->handle();
+            });
     }
 }
