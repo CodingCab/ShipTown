49f3c485bb5861a9780445da4a4c0e9dabe0044c
Artur Hanusek <arthur@youritsolutions.ie>
2024-08-09 20:04:35 +0100

    update schema dump and its handling

--- app/Listeners/SchemaLoadedListener.php
+++ app/Listeners/SchemaLoadedListener.php
@@ -2,15 +2,12 @@
 
 namespace App\Listeners;
 
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\DB;
+use App\Console\Commands\AppInstall;
 
 class SchemaLoadedListener
 {
     public function handle($event): void
     {
-        DB::transaction(function () {
-            Artisan::call('app:install');
-        });
+        AppInstall::ensureAppKeysGenerated();
     }
 }
