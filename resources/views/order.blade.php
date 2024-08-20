001bbdb0b2e25571e5c015d6452402e1a2338cb8
Nikola Radovanović <admin@nrgsourcecode.com>
2024-08-20 16:35:56 +0200

    Added orders report

---
+++ resources/views/order.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title',__('Orders'))
+
+@section('content')
+    <report
+        meta-string="{{ json_encode($meta) }}"
+        record-string="{{ json_encode($data) }}"
+        download-button-text="{{ __('Download All') }}"
+        :show-barcode-input="false"
+    ></report>
+@endsection
