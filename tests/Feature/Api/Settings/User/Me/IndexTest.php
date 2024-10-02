e41e865a61c5d1a44364bbfa73b45e49e5417750
Artur Hanusek <arthur@youritsolutions.ie>
2024-10-02 23:49:58 +0200

    refactoring

--- tests/Feature/Api/Settings/User/Me/IndexTest.php
+++ tests/Feature/Api/Settings/User/Me/IndexTest.php
@@ -12,7 +12,7 @@ class IndexTest extends TestCase
     {
         $user = User::factory()->create();
 
-        $response = $this->actingAs($user, 'api')->getJson(route('api.users.me.index'));
+        $response = $this->actingAs($user, 'api')->getJson(route('api.user.me.index'));
 
         $response->assertOk();
         $response->assertJsonStructure([
