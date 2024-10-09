e4b59745dadc2ef38e4a7b6cda6b807a9e2640ff
Artur Hanusek <arthur@youritsolutions.ie>
2024-10-10 00:57:18 +0200

    update test

--- tests/Browser/Routes/Settings/Modules/MagentoApiPageTest.php
+++ tests/Browser/Routes/Settings/Modules/MagentoApiPageTest.php
@@ -3,8 +3,6 @@
 namespace Tests\Browser\Routes\Settings\Modules;
 
 use App\Modules\MagentoApi\src\EventServiceProviderBase;
-use App\User;
-use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 use Throwable;
 
@@ -20,25 +18,5 @@ class MagentoApiPageTest extends DuskTestCase
         EventServiceProviderBase::enableModule();
 
         $this->basicAdminAccessTest($this->uri, true);
-        $this->basicUserAccessTest($this->uri, false);
-        $this->basicGuestAccessTest($this->uri);
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function testPage(): void
-    {
-        EventServiceProviderBase::enableModule();
-
-        /** @var User $user */
-        $user = User::factory()->create();
-        $user->assignRole('admin');
-
-        $this->browse(function (Browser $browser) use ($user) {
-            $browser->loginAs($user)
-                ->visit($this->uri)
-                ->assertSee('Magento Api Configurations');
-        });
     }
 }
