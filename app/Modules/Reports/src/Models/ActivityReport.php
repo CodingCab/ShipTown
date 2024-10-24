207f38cb0f1cdd490a917323822f5b2d1f49b4b8
Rizky Puji Raharja <rizky.rsj@gmail.com>
2024-10-24 15:19:28 +0700

    show causer name (#667)

--- app/Modules/Reports/src/Models/ActivityReport.php
+++ app/Modules/Reports/src/Models/ActivityReport.php
@@ -14,7 +14,11 @@ class ActivityReport extends Report
         $this->report_name = 'Activity Log';
         $this->defaultSort = '-id';
 
-        $this->baseQuery = Activity::query();
+        $this->baseQuery = Activity::query()
+            ->leftJoin('users', function ($join) {
+                $join->on('activity_log.causer_id', '=', 'users.id')
+                    ->where('activity_log.causer_type', 'App\User');
+            });
 
         $this->fields = [
             'id' => 'activity_log.id',
@@ -24,6 +28,7 @@ class ActivityReport extends Report
             'subject_id' => 'activity_log.subject_id',
             'causer_id' => 'activity_log.causer_id',
             'causer_type' => 'activity_log.causer_type',
+            'user_name' => 'users.name',
             'properties' => 'activity_log.properties',
         ];
 
